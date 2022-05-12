98827b3b39c9bcd110fe5eea6c55312edbecaa49
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-05-12 14:23:00 +0200

    MenyHelper

---
+++ Employee.Helpers/MenyHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Helpers
+{
+    public class MenyHelper
+    {
+        public const string Add = "1";
+        public const string Show = "2";
+        public const string Quit = "Q";
+    }
+}
